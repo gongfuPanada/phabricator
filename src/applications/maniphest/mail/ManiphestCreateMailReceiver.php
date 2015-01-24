6c463c114812cb85d6916b9a5dc8dbeb5bc3b75b
David Kravitz <dnkravitz@gmail.com>
2015-01-24 00:01:52 +0000

    Generalize privacy incidents mail checking code

--- src/applications/maniphest/mail/ManiphestCreateMailReceiver.php
+++ src/applications/maniphest/mail/ManiphestCreateMailReceiver.php
@@ -10,15 +10,27 @@ final class ManiphestCreateMailReceiver extends PhabricatorMailReceiver {
   public function canAcceptMail(PhabricatorMetaMTAReceivedMail $mail) {
     $config_key = 'metamta.maniphest.public-create-email';
     $create_address = PhabricatorEnv::getEnvConfig($config_key);
-    $privacy_incidents_address = 'dummy-list@snapchat.com';
 
     foreach ($mail->getToAddresses() as $to_address) {
-      if ($this->matchAddresses($create_address, $to_address) ||
-          $this->matchAddresses($privacy_incidents_address, $to_address)) {
+      if ($this->matchAddresses($create_address, $to_address)) {
         return true;
       }
     }
 
+    if (ManiphestCreateMailReceiver::isPrivacyIncident($mail)) {
+        return true;
+    }
+
+    return false;
+  }
+
+  public static function isPrivacyIncident(PhabricatorMetaMTAReceivedMail $mail) {
+    $privacy_incidents_address = 'dummy-list@snapchat.com';
+    foreach ($mail->getToAddresses() as $to_address) {
+      if (PhabricatorMailReceiver::matchAddresses($privacy_incidents_address, $to_address)) {
+        return true;
+      }
+    }
     return false;
   }
 
